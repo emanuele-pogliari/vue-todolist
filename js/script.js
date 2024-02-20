494947245a681babd9d2b0d8496ee6729a0a793f
emanuele-pogliari <manu.pogliari@gmail.com>
2024-02-20 15:51:26 +0100

    Milestone 1 completed

--- js/script.js
+++ js/script.js
@@ -2,23 +2,25 @@ const { createApp } = Vue
 
 createApp({
     data() {
-        tasks: [
-            {
-                text: 'task 1',
-                done: true
-            }, {
-                text: 'task 2',
-                done: false
-            }, {
-                text: 'task 3',
-                done: true
-            }, {
-                text: 'task 4',
-                done: false
-            }, {
-                text: 'task 5',
-                done: false
-            }
-        ]
+        return {
+            tasks: [
+                {
+                    text: 'task 1',
+                    done: true
+                }, {
+                    text: 'task 2',
+                    done: false
+                }, {
+                    text: 'task 3',
+                    done: true
+                }, {
+                    text: 'task 4',
+                    done: false
+                }, {
+                    text: 'task 5',
+                    done: false
+                }
+            ]
+        }
     }
 }).mount('#app');
